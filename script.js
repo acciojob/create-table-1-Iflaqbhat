72cf50d1e2ff8abf2f51898d246310d6aa1fce0d
Iflaq khurshid <112414810+Iflaqbhat@users.noreply.github.com>
2024-11-02 11:49:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 function insert_Row() {
+	let count=3;
     //Write your code here
-	let elements = document.querySelectorAll(".c");
-    elem.style.display = "block"; // or another display style as needed
+	let elements = document.querySelectorAll("#sampleTable");
+
+    element.innerHTML += `
+        <tr>
+            <td>Row${count} cell1</td>
+            <td>Row${count} cell2</td>
+        </tr>
+    `;
 });
+ 
+	// or another display style as needed
+
   
   
 }
