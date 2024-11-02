9c355b179088e7a0a11e91a5c5c7a4a156a8b09a
Iflaq khurshid <112414810+Iflaqbhat@users.noreply.github.com>
2024-11-02 11:34:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function insert_Row() {
     //Write your code here
+	let elements = document.querySelectorAll(".c");
+    elem.style.display = "block"; // or another display style as needed
+});
   
   
 }
